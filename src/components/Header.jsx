335746ab89a3d4cf2151014224cc7f926f71e816
Draz6 <163674931+Draz6@users.noreply.github.com>
2024-06-21 16:04:16 +0300

    update links

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,7 @@
 
 import PersonIcon from "@mui/icons-material/Person";
 import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
+import { Link } from "react-router-dom";
 import '../styles/Header.css';
 
 export default function Header() {
@@ -10,9 +11,9 @@ export default function Header() {
                 <h4 id="logo">D R &forall; Z</h4>
             </ul>
             <ul className='menu'>
-                <a href="/LogIn"><li>Home</li></a>
+                <a href="/LogIn"><li><Link to={"/LogIn"}>Home</Link></li></a>
                 <li>Collection</li>
-                <a href="/LogIn"> <li><PersonIcon /></li></a>
+                <li><Link to={"/LogIn"}><PersonIcon /></Link></li>
                 <li><ShoppingCartIcon /></li>
             </ul>
         </div>
